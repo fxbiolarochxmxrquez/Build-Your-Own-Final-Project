8c046adf6c4a03f0cee89b112f833b3aac23a370
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-04-12 10:00:40 -0500

    Initial Commit

---
+++ Build Your Own Final Project/Build_Your_Own_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Build_Your_Own_Final_ProjectApp.swift
+//  Build Your Own Final Project
+//
+//  Created by Fabiola Rocha Marquez on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Build_Your_Own_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Build Your Own Final Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Build Your Own Final Project
+//
+//  Created by Fabiola Rocha Marquez on 4/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
