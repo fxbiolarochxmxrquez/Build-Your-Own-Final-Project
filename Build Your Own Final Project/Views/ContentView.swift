540d59a7fcd7d9261161167c3581ad5a40179aaf
Keila Islas <kislas@bsd220.org>
2024-04-22 10:08:39 -0500

    Began home page

---
+++ Build Your Own Final Project/Views/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  Build Your Own Final Project
+//
+//  Created by Fabiola Rocha Marquez on 4/12/24.
+//
+
+// Keila's comment
+// Ariya's comment
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.blue.opacity(0.7).ignoresSafeArea(.all)
+            VStack {
+                Text("Find Your Match!")
+                    .font(.title).bold()
+                Spacer()
+            }
+                NavigationLink {
+                    GameView()
+                } label: {
+                    CustomButton(text: "Play")
+                
+                }
+            .padding()
+            }
+        }
+    }
+}
+struct CustomButton: View {
+    var text: String
+    var background: Color = .yellow
+    var body: some View {
+        Text(text)
+            .foregroundColor(.cyan)
+            .padding()
+            .padding(.horizontal)
+            .background(background)
+            .shadow(radius: 10)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Build Your Own Final Project/Views/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  GameView.swift
+//  Build Your Own Final Project
+//
+//  Created by Keila Islas on 4/22/24.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameView()
+}
