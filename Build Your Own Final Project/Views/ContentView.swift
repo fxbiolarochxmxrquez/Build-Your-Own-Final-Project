f63b4a2f04c9dc36cb808ef069d10deb89609159
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-04-23 16:19:29 -0500

    fixed "Play" button layout

--- Build Your Own Final Project/Views/ContentView.swift
+++ Build Your Own Final Project/Views/ContentView.swift
@@ -11,42 +11,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     var body: some View {
         NavigationView {
             ZStack {
-                Color.blue.opacity(0.7).ignoresSafeArea(.all)
-            VStack {
-                Text("Find Your Match!")
-                    .font(Font.custom("Marker Felt", size: 35))
-                    Spacer()
-                
-                Image("memory")
-                    .resizable()
-                    .frame(width: 170, height: 150)
-                Spacer()
-                Spacer()
-                Spacer()
-                
-            }
-                NavigationLink {
-                    GameView()
-                } label: {
-                    CustomButton(text: "Play")
-                        .cornerRadius(25)
-                        .shadow(radius: 20)
+                Color.blue.opacity(0.7).ignoresSafeArea()
+                VStack {
+                    VStack {
+                        Text("Find Your Match!")
+                            .font(Font.custom("Marker Felt", size: 35))
+                        Image("memory")
+                            .resizable()
+                            .frame(width: 170, height: 150)
+                            .cornerRadius(20)
+                    }
+                    NavigationLink {
+                        GameView()
+                    } label: {
+                        CustomButton(text: "Play")
+                            .cornerRadius(25)
+                            .shadow(radius: 20)
+                    }
                 }
-                .padding()
             }
         }
     }
 }
+
 struct CustomButton: View {
     var text: String
     var background: Color = .yellow
     var body: some View {
         Text(text)
-            .foregroundColor(.cyan)
+            .foregroundColor(.blue)
             .padding()
             .padding(.horizontal)
             .background(background)
