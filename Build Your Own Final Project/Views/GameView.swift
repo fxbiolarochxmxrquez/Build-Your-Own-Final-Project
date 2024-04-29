6eaff3808df4fff73bf3df3d791cb58b75735a7d
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-04-29 10:07:36 -0500

    began card flip feature

--- Build Your Own Final Project/Views/GameView.swift
+++ Build Your Own Final Project/Views/GameView.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 struct GameView: View {
+    @State var isFaceUp = false
     var body: some View {
         NavigationView {
             ZStack {
-                Color.blue.opacity(0.7)
-                    .ignoresSafeArea()
+                Color.blue.opacity(0.7).ignoresSafeArea()
                 VStack {
                     Text("4x4")
                         .font(Font.custom("Marker Felt", size: 35))
@@ -20,38 +20,22 @@ struct GameView: View {
                         .font(Font.custom("Marker Felt", size: 20))
                     Spacer()
                     HStack {
-                        ZStack(content:{
-                            RoundedRectangle(cornerRadius: 12)
-                                .foregroundColor(.white)
-                            RoundedRectangle(cornerRadius: 12)
-                                .strokeBorder(lineWidth: 2)
-                            Image("mickey").font(.largeTitle)
-                        })
-                        ZStack(content:{
-                            RoundedRectangle(cornerRadius: 12)
-                                .foregroundColor(.white)
-                            RoundedRectangle(cornerRadius: 12)
-                                .strokeBorder(lineWidth: 2)
-                            Image("mickey").font(.largeTitle)
-                        })
-                        ZStack(content:{
-                            RoundedRectangle(cornerRadius: 12)
-                                .foregroundColor(.white)
-                            RoundedRectangle(cornerRadius: 12)
-                                .strokeBorder(lineWidth: 2)
-                            Image("mickey").font(.largeTitle)
-                        })
-                        ZStack(content:{
-                            RoundedRectangle(cornerRadius: 12)
-                                .foregroundColor(.white)
-                            RoundedRectangle(cornerRadius: 12)
-                                .strokeBorder(lineWidth: 2)
-                            Image("mickey").font(.largeTitle)
-                        })
+                        ZStack {
+                            let base = RoundedRectangle(cornerRadius: 12)
+                            if isFaceUp {
+                                base.foregroundColor(.white)
+                                base.strokeBorder(lineWidth: 2)
+                                Image("mickey").resizable()
+                            } else {
+                                base.fill()
+                            }
+                        }
+                        .onTapGesture {
+                            isFaceUp.toggle()
+                        }
                     }
                 }
             }
-           
         }
     }
 }
