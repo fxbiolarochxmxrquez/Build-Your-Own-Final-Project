ec95abbfc793c7259f524f78a3b12de6c14ef1db
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-05-09 23:11:35 -0500

    cards are matchable

--- Build Your Own Final Project/Views/GameView.swift
+++ Build Your Own Final Project/Views/GameView.swift
@@ -18,10 +18,14 @@ struct GameView: View {
                 Text("Disney Characters")
                     .font(Font.custom("Marker Felt", size: 20))
                 LazyVGrid(columns: [GridItem(.adaptive(minimum: 88), spacing: 0)], spacing: 0) {
-                    ForEach(viewModel.cards.indices, id: \.self) { index in
-                        CardView(viewModel.cards[index])
+                    ForEach(viewModel.cards) { card in
+                        CardView(card)
                             .aspectRatio(2/3, contentMode: .fit)
                             .padding(4)
+                            .onTapGesture {
+                                viewModel.choose(card)
+                            }
+                            .animation(.default, value: viewModel.cards)
                     }
                 }
                 Button("Shuffle") {
@@ -49,6 +53,7 @@ struct CardView: View {
             .opacity(card.isFaceUp ? 1 : 0)
             base.foregroundColor(.yellow).opacity(card.isFaceUp ? 0 : 1)
         }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
 }
 
