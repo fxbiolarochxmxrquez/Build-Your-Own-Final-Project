a891babf94f805a7ce4410a558867e75d5ed7be1
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-05-22 10:16:16 -0500

    animation pt. 1 added score flip card animation

---
+++ Build Your Own Final Project/Funtionalities/FlyingNumber.swift
@@ -0,0 +1,18 @@
+//
+//  FlyingNumber.swift
+//  Build Your Own Final Project
+//
+//  Created by Fabiola Rocha Marquez on 5/22/24.
+//
+
+import SwiftUI
+
+struct FlyingNumber: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlyingNumber()
+}
